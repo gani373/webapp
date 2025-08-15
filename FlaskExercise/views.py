fce779962c0babe0336ee98d4ec08b2037b06e5a
gani373 <gg3466000@gmail.com>
2025-08-15 15:12:51 +0530

    Update views.py

--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -85,7 +85,7 @@ def _build_msal_app(cache=None, authority=None):
     return msal.ConfidentialClientApplication(
     Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
     client_credential=Config.CLIENT_SECRET, token_cache=cache)
-    return N
+    
 
 
 def _build_auth_url(authority=None, scopes=None, state=None):
@@ -94,4 +94,4 @@ def _build_auth_url(authority=None, scopes=None, state=None):
     scopes or [],
     state=state or str(uuid.uuid4()),
     redirect_uri=url_for('authorized', _external=True, _scheme='https'))
-    return None
+    
